84549c95d9be73b957377968b3e8174f4e67df2b
Yash Djson Dookun <dookun88@gmail.com>
2020-04-09 11:45:29 +0400

    Add files via upload

---
+++ Intersection/intersection.py
@@ -0,0 +1,32 @@
+'''
+	Given 2 Arrays, write a function to compute their intersection - the intersection means the numbers that are in both arrays
+
+	Note:
+	Each element in the result must be unique.
+	The result can be in any order
+
+	EXAMPLE 1:
+	Input: nums1 = [1,2,2,1], nums2 = [2,2]
+	Output: [2]
+
+	
+	Example 2:
+	Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+	Output: [9,4]
+'''
+
+
+class Intersection:
+	def intersection(self, nums1, nums2):
+		commons = []
+
+		for i in range (len(nums2)):
+			for j in range (len(nums1)):
+				if nums2[i] == nums1[j]:
+					commons.append(nums1[j])
+
+		commons = list(dict.fromkeys(commons))
+		return commons
+		
+# print(Intersection().intersection([4,9,5],[9,4,9,8,4]))
+# print(Intersection().intersection([1,2,2,1],[2,2]))
