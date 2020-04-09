1c77e17bbef176ad52ae4863ca7ba5f63cadd36b
Yash Djson Dookun <dookun88@gmail.com>
2020-04-09 11:52:56 +0400

    Update intersection.py

--- Intersection/intersection.py
+++ Intersection/intersection.py
@@ -9,7 +9,6 @@
 	Input: nums1 = [1,2,2,1], nums2 = [2,2]
 	Output: [2]
 
-	
 	Example 2:
 	Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
 	Output: [9,4]
@@ -19,12 +18,10 @@
 class Intersection:
 	def intersection(self, nums1, nums2):
 		commons = []
-
 		for i in range (len(nums2)):
 			for j in range (len(nums1)):
 				if nums2[i] == nums1[j]:
 					commons.append(nums1[j])
-
 		commons = list(dict.fromkeys(commons))
 		return commons
 		
